7a21316a78bd47b98d8c0ce06cfeec69b3b44477
Rongyeye <760639813@qq.com>
2020-07-05 19:54:10 +0800

    code vuejs.org document

---
+++ learnJs/vueDocumentCode/components/blogPost.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:['title'],
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
